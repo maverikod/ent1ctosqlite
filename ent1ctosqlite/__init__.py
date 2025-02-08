ea5f2ae835b0139c4a3d12b1c3b5d48f7284a3fa
Vasiliy Zdanovskiy <vasilyvz@gmail.com>
2025-02-09 00:27:37 +0200

    Update author information and package settings

--- ent1ctosqlite/__init__.py
+++ ent1ctosqlite/__init__.py
@@ -32,8 +32,8 @@ from .utils import (
 )
 
 __version__ = '0.1.0'
-__author__ = 'Your Name'
-__email__ = 'your.email@example.com'
+__author__ = 'maverikod'
+__email__ = 'vasilyvz@gmail.com'
 
 # Настройка логгера по умолчанию
 import logging
